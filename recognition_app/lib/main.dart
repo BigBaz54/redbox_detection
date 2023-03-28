b7b36575a726317a3c97efd4d83a2cb88ff9ecde
BigBaz <louisvincent@famillecapelli.fr>
2023-03-29 00:42:09 +0200

    [+] importing model

--- recognition_app/lib/main.dart
+++ recognition_app/lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
 import 'home_page.dart';
-
+import 'package:flutter_pytorch/flutter_pytorch.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final cameras = await availableCameras();
-  runApp(MyApp(cameras: cameras));
+  final List<CameraDescription> cameras = await availableCameras();
+  final ModelObjectDetection model = await loadModel();
+  runApp(MyApp(cameras: cameras, objectModel: model));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({required this.cameras, Key? key}) : super(key: key);
+  const MyApp({required this.cameras, required this.objectModel, Key? key}) : super(key: key);
 
   final List<CameraDescription> cameras;
+  final ModelObjectDetection objectModel;
 
   @override
   Widget build(BuildContext context) {
@@ -22,5 +25,23 @@ class MyApp extends StatelessWidget {
       ),
       home: HomePage(cameras: cameras),
     );
+  }
+
+}
+
+Future loadModel() async {
+  String pathObjectDetectionModel = "assets/models/best.torchscript";
+  try {
+    final ModelObjectDetection objectModel = await FlutterPytorch.loadObjectDetectionModel(
+              pathObjectDetectionModel, 1, 640, 640,
+              labelPath: "assets/labels/labels.txt");
+    return objectModel;
+  } catch (e) {
+    if (e is PlatformException) {
+      print("Only supported for android, Error is $e");
+    } else {
+      print("Error is $e");
+    }
+    return null;
   }
 }
